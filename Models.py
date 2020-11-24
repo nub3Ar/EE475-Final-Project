4ba34028886d450c3715cf2cf4c4737567281682
karinazad <karinazadorozhny2022@u.northwestern.edu>
2020-11-24 17:22:27 +0100

    Added Models.py

---
+++ Models.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+
+# Good comparison of the MNIST classification models:
+# http://rodrigob.github.io/are_we_there_yet/build/classification_datasets_results.html
+
+# and Baldominos 2019: https://www.researchgate.net/publication
+# /334957576_A_Survey_of_Handwritten_Character_Recognition_with_MNIST_and_EMNIST
+
+
+class CNN_Dropout:
+
+    def __init__(self):
+        pass
+
+
+class CNN_DropCon:
+
+    def __init__(self):
+        pass
+
+
+
+class CNN_BN_Maxout:
+
+    def __init__(self):
+        pass
+
+
+
