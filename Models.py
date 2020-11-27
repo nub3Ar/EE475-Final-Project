3e279c2a6843f3fac49ef496b43849342b22acfb
karinazad <karinazadorozhny2022@u.northwestern.edu>
2020-11-27 20:45:12 +0100

    Add CNN and DropConnect

--- Models.py
+++ Models.py
@@ -1,30 +1,55 @@
-import tensorflow as tf
-import numpy as np
+import keras
+from Layers import DropConnect
 
-# Good comparison of the MNIST classification models:
-# http://rodrigob.github.io/are_we_there_yet/build/classification_datasets_results.html
+INPUT_SHAPE = (28,28,1)
+NUM_CLASSES = 10
 
-# and Baldominos 2019: https://www.researchgate.net/publication
-# /334957576_A_Survey_of_Handwritten_Character_Recognition_with_MNIST_and_EMNIST
-
-
-class CNN_Dropout:
+class CNN(keras.Model):
 
     def __init__(self):
-        pass
+        super(CNN, self).__init__()
+        self.model = keras.models.Sequential()
+        self.model.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1, padding='Same', activation='relu',
+                                      input_shape = INPUT_SHAPE) )
+        self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2), padding='same'))
+        self.model.add(keras.layers.Conv2D(filters=64,kernel_size=(3, 3),strides=1,padding='same', activation='relu'))
+        self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2),padding='same'))
+        self.model.add(keras.layers.Flatten())
+        self.model.add(keras.layers.Dense(units=NUM_CLASSES, activation='sigmoid'))
 
+    def call(self, inputs):
+        return self.model(inputs)
 
-class CNN_DropCon:
+class CNN_Drop(keras.Model):
 
     def __init__(self):
-        pass
+        super(CNN_DropC, self).__init__()
+        self.model = keras.models.Sequential()
+        self.model.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1, padding='Same', activation='relu',
+                                      input_shape = INPUT_SHAPE) )
+        self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2), padding='same'))
+        self.model.add(keras.layers.Conv2D(filters=64,kernel_size=(3, 3),strides=1,padding='same', activation='relu'))
+        self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2),padding='same'))
+        self.model.add(keras.layers.Flatten())
+        self.model.add(keras.layers.Dropout(0.5))
+        self.model.add(keras.layers.Dense(units=NUM_CLASSES, activation='sigmoid'))
 
+    def call(self, inputs):
+        return self.model(inputs)
 
-
-class CNN_BN_Maxout:
+class CNN_DropC(keras.Model):
 
     def __init__(self):
-        pass
+        super(CNN_DropC, self).__init__()
+        self.model = keras.models.Sequential()
+        self.model.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1, padding='Same', activation='relu',
+                                      input_shape = INPUT_SHAPE) )
+        self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2), padding='same'))
+        self.model.add(keras.layers.Conv2D(filters=64,kernel_size=(3, 3),strides=1,padding='same', activation='relu'))
+        self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2),padding='same'))
+        self.model.add(keras.layers.Flatten())
+        self.model.add(DropConnect(keras.layers.Dense(64, activation='relu'), prob=0.5),
+        self.model.add(keras.layers.Dense(units=NUM_CLASSES, activation='sigmoid'))
 
-
-
+    def call(self, inputs):
+        return self.model(inputs)
