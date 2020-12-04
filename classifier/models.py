d7f936e8ec561995d3416a489e8f3a5463999902
nub3Ar <barryz@Barry-Z.localdomain>
2020-12-03 18:05:43 -0600

    added pre-trained model and prediction functinos

--- classifier/models.py
+++ classifier/models.py
@@ -1,5 +1,5 @@
 import keras
-from classifier.layers import DropConnect
+from layers import DropConnect
 
 INPUT_SHAPE = (28,28,1)
 NUM_CLASSES = 10
@@ -23,7 +23,7 @@ class CNN(keras.Model):
 class CNN_Drop(keras.Model):
 
     def __init__(self):
-        super(CNN_DropC, self).__init__()
+        super(CNN_Drop, self).__init__()
         self.model = keras.models.Sequential()
         self.model.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1, padding='Same', activation='relu',
                                       input_shape = INPUT_SHAPE) )
@@ -37,23 +37,26 @@ class CNN_Drop(keras.Model):
     def call(self, inputs):
         return self.model(inputs)
 
-class CNN_DropC(keras.Model):
+class CNN_BN(keras.Model):
 
     def __init__(self):
-        super(CNN_DropC, self).__init__()
+        super(CNN_BN, self).__init__()
         self.model = keras.models.Sequential()
-        self.model.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1, padding='Same', activation='relu',
-                                            input_shape = INPUT_SHAPE))
+        self.model.add(keras.layers.Conv2D(filters=32, kernel_size=(3, 3), strides=1, padding='Same', input_shape = INPUT_SHAPE) )
+        self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.ReLU())
         self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2), padding='same'))
-        self.model.add(keras.layers.Conv2D(filters=64,kernel_size=(3, 3),strides=1,padding='same', activation='relu'))
+        
+        self.model.add(keras.layers.Conv2D(filters=64,kernel_size=(3, 3),strides=1,padding='same'))
+        self.model.add(keras.layers.BatchNormalization())
+        self.model.add(keras.layers.ReLU())
         self.model.add(keras.layers.MaxPool2D(pool_size=(2, 2),padding='same'))
+        
         self.model.add(keras.layers.Flatten())
-        self.model.add(DropConnect(keras.layers.Dense(64, activation='relu'), prob=0.5)),
         self.model.add(keras.layers.Dense(units=NUM_CLASSES, activation='sigmoid'))
 
     def call(self, inputs):
         return self.model(inputs)
 
-
-def try_connect():
-    return "Connection to models"
+if __name__ == '__main__':
+    print('model import working')
