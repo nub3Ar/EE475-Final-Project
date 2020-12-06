27924613fd708e39f2579562130ef06440cf6adf
nub3Ar <barryz@Barry-Z.localdomain>
2020-12-06 00:00:51 -0600

    final

--- utils/util_function.py
+++ utils/util_function.py
@@ -63,3 +63,18 @@ def plot_accuracy(histories, titles, ):
         axes[i].xaxis.set_major_locator(MaxNLocator(integer=True))
 
     plt.show()
+
+def process_data(x_train, y_train, x_test, y_test):
+    
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train_, num_classes)
+    y_test = keras.utils.to_categorical(y_test_, num_classes)
+
+    return x_train, y_train, x_test, y_test
